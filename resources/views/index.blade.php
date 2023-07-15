efa286e78a079f50897eef23bb6e44f783029946
ushijima eisaku <eisakuushijima@gmail.com>
2023-07-15 21:51:49 +0900

    tailwindの適用。共通テンプレートの作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gofuku Lunch</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <h1>お店一覧</h1>
 
     @foreach ($shops as $shop)
@@ -17,5 +11,4 @@
             {{ $shop->distance_from_company }}
         </p>
     @endforeach
-</body>
-</html>
+@endsection
