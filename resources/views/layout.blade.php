5ccb0fe00c7b357f7700bc2cdf4de2f079c40124
ushijima eisaku <eisakuushijima@gmail.com>
2023-07-16 11:34:06 +0900

    tailwind daisyui googlefontの適用

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gofuku Lunch</title>
-    <style>body {padding: 80px;}</style>
+    <style>body {padding-top: 80px;
+    font-family: 'Noto Sans JP', sans-serif;}
+
+    </style>
     @vite('resources/css/app.css')
 </head>
 <body>
     <h1 class="text-3xl font-bold underline">
-        Hello world!
+        Hello world! おはよう世界
     </h1>
     @yield('content')
 </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,35 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            raleway: ['Raleway'],
+            kosugimaru: ['Kosugi Maru'],
+            azeretmono: ['Azeret Mono'],
+          },
+    },
+
   },
-  plugins: [],
+  daisyui: {
+      themes: [
+        {
+            mytheme: {
+                "primary": "#45f777",
+                "secondary": "#06a51b",
+                "accent": "#baf48d",
+                "neutral": "#151823",
+                "base-100": "#f0eff0",
+                "info": "#45baed",
+                "success": "#7be0c0",
+                "warning": "#f4bb57",
+                "error": "#f92468",
+            },
+        },
+      ],
+  },
+  plugins: [require("daisyui")],
 }
 
